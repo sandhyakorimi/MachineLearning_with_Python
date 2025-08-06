a087214a8c719d8ae4d4f9cd5aed19801596e8cf
SANDHYA <24P31F00D4@acet.ac.in>
2025-08-06 11:07:55 +0530

    String_Slice

---
+++ Slicing_Strings.py
@@ -0,0 +1,10 @@
+string = 'ASTRING'
+S1 = slice(3)
+S2 = slice(1,5,2)
+S3 = slice(-1,-12,-2)
+print("String Slicing")
+print(string[S1])
+print(string[S2])
+print(string[S3])
+lst = [98,78,90,50,33,21,34,64,28,23]
+print(lst[7::-1])
